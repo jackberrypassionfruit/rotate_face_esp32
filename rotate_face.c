c94d79fdfcc632787790c75dc8d3a2cbf568acf9
jackberrypassionfruit <pashayanjack@gmail.com>
2024-05-19 17:14:18 -0400

    idfk

--- rotate_face.c
+++ rotate_face.c
@@ -2,7 +2,7 @@
 
 int test_row_int[8] =       { 1,2,3,4,5,6,7,8 };
 char test_row_char[8] =     "fuckbutt";
-char test_2d_row[3][3] =    { "abc", "def", "ghi" };
+char* test_2d_row[3] =    { "abc", "def", "ghi" };
 char happy_face[8][8] =     { "00111100", "01000010", "10100101", "10000001", "10100101", "10011001", "01000010", "00111100" };
 char neutral_face[8][8] =   { "00111100", "01000010", "10100101", "10000001", "10111101", "10000001", "01000010", "00111100" };
 char sad_face[8][8] =       { "00111100", "01000010", "10100101", "10000001", "10011001", "10100101", "01000010", "00111100" };
@@ -37,7 +37,7 @@ void reverse_char_row(char* row) {
 
 void rotate_2d_array(char** rows) {
     char rotated_rows[3][3];
-
+    // cache copy of original rows
     for (int i = 0; i < 3; i++) {
         // each row of input
         // each col of output
@@ -47,8 +47,25 @@ void rotate_2d_array(char** rows) {
             // each row of output
             rotated_rows[j][i] = *(this_row_ptr + j*sizeof(char));
         }
-
     }
+    // // rotate rows inplace, using cache
+    // for (int i = 0; i < 3; i++) {
+    //     // each row of input
+    //     // each col of output
+    //     char* this_row_ptr = rows + i*3*sizeof(char);
+    //     for (int j = 0; j < 3; j++) {
+    //         // each col of input
+    //         // each row of output
+    //         // printf("i: %i, j: %i\n",i, j);
+    //         *(this_row_ptr + j*sizeof(char)) = rotated_rows[j][i];
+    //     }
+    // }
+
+
+    printf("{\n"); for (int i = 0; i < 3; i++) {
+        printf("%s,\n", rotated_rows[i]);
+    }
+    printf("}\n");
 }
 
 void main() {
@@ -65,4 +82,10 @@ void main() {
     // printf("}\n");
 
     rotate_2d_array(test_2d_row);
+    // printf("test_2d_row[0]: %s\n", test_2d_row[0]);
+
+    // printf("{\n"); for (int i = 0; i < 3; i++) {
+    //     printf("%s,\n", test_2d_row[i]);
+    // }
+    // printf("}\n");
 }
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+sing cache
+    // for (int i = 0; i < 3; i++) {
+    //     // each row of input
+    //     // each col of output
+    //     char* this_row_ptr = rows + i*3*sizeof(char);
+    //     for (int j = 0; j < 3; j++) {
+    //         // each col of input
+    //         // each row of output
+    //         // printf("i: %i, j: %i\n",i, j);
+    //         *(this_row_ptr + j*sizeof(char)) = rotated_rows[j][i];
+    //     }
+    // }
